dbabe42f2e29931a8e0fa719369871a25623a450
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2025-08-13 16:32:42 +0200

    Set $EPICS_HOST_ARCH using $target_platform

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,14 @@ cat <<EOF > configure/RELEASE.local
 EPICS_BASE=${EPICS_BASE}
 EOF
 
+if [[ "$target_platform" == "osx-arm64" ]]; then
+  export EPICS_HOST_ARCH="darwin-aarch64"
+elif [[ "$target_platform" == "osx-64" ]]; then
+  export EPICS_HOST_ARCH="darwin-x86"
+else
+  export EPICS_HOST_ARCH=$(perl ${EPICS_BASE}/lib/perl/EpicsHostArch.pl)
+fi
+
 make -j ${CPU_COUNT}
 
 install -d ${PREFIX}/bin/
