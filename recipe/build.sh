812db5e4bada840adc521aa2b328f29b08b1de68
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2025-08-14 12:39:48 +0200

    Remove superfluous elif for EPICS_HOST_ARCH

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,6 @@ EOF
 
 if [[ "$target_platform" == "osx-arm64" ]]; then
   export EPICS_HOST_ARCH="darwin-aarch64"
-elif [[ "$target_platform" == "osx-64" ]]; then
-  export EPICS_HOST_ARCH="darwin-x86"
 else
   export EPICS_HOST_ARCH=$(perl ${EPICS_BASE}/lib/perl/EpicsHostArch.pl)
 fi
